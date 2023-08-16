52b3c56568d977cb2b148b3a0b8f93eb7238ba08
Danya <danya@JOD0266L-2.local>
2023-08-16 12:24:40 +0300

    added general api and fix arrows

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -9,6 +9,35 @@ class ApiService {
   final Dio _dio = Dio(BaseOptions(baseUrl: baseUrl));
   List<Country> _countries = [];
 
+  Future<T> apiRequest<T>(
+      {required String path,
+      dynamic body,
+      required String method,
+      Options? options}) async {
+    try {
+      Response response;
+
+      if (method == 'GET') {
+        response = await _dio.get(path, options: options);
+      } else if (method == 'POST') {
+        response = await _dio.post(path, data: body, options: options);
+      } else {
+        throw Exception('Unsupported HTTP method: $method');
+      }
+
+      if (response.statusCode == 200) {
+        return response.data;
+        // return response.data as T;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      HttpExceptionHandler.handleException(e);
+      print(e);
+      throw Exception(e.toString());
+    }
+  }
+
   Future<List<Country>> getCountriesData() async {
     try {
       String language = locator<HiveService>()
