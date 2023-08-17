27988aeef0eb80b27be33a88a349ba540c7a719a
Eleana <eleana@JOD0267L-2.local>
2023-08-17 10:20:56 +0300

    equiti screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'locater.dart';
-import 'screens/equiti/equiti_screen.dart';
+import 'screens/equiti/equiti_academy_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -57,7 +57,7 @@ class MainAppState extends State<MainApp> {
           Locale(arLocale),
         ],
         debugShowCheckedModeBanner: false,
-        home: const EquitiAcadmyScreen());
+        home: const StartupScreen());
   }
 
   void rebuild() {
