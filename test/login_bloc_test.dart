d2001ea9ac2c9eb7d43c6d208732786ee40d5693
Eleana <eleana@JOD0267L-2.local>
2023-08-21 16:08:32 +0300

    validate phone number all scenarios added

--- test/login_bloc_test.dart
+++ test/login_bloc_test.dart
@@ -2,10 +2,35 @@ import 'package:countries_app/screens/login/login_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('test phone number length validator', () {
-    final loginBloc = LoginBLoc();
+  group('test phone number length validator', () {
+    test('test phone number length < min len  validator return error message',
+        () {
+      final loginBloc = LoginBLoc();
 
-    var val = loginBloc.validateNumber('797123098', 8);
-    expect(val, 'Please enter 8 digits number');
+      var val = loginBloc.validateNumber('797123098', 10);
+      expect(val, 'Please enter 10 digits number');
+    });
+    test(
+        'test phone number length/empty < min len  validator return error message',
+        () {
+      final loginBloc = LoginBLoc();
+
+      var val = loginBloc.validateNumber('', 10);
+      expect(val, 'Please enter 10 digits number');
+    });
+    test('test phone number length = min len validator return null', () {
+      final loginBloc = LoginBLoc();
+
+      var val = loginBloc.validateNumber('797123098', 9);
+      expect(val, null);
+    });
+// impossible scenario
+    test('test phone number length > min len  validator return error message',
+        () {
+      final loginBloc = LoginBLoc();
+
+      var val = loginBloc.validateNumber('797123098', 2);
+      expect(val, 'Please enter 2 digits number');
+    });
   });
 }
