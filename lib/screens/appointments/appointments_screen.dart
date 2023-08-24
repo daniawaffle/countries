29e984fc812f7fc0a9e1cd79a7ace91de9bdb8b3
Danya <danya@JOD0271L.local>
2023-08-24 12:16:57 +0300

    fixed merged mistake in appointment screen

--- lib/screens/appointments/appointments_screen.dart
+++ lib/screens/appointments/appointments_screen.dart
@@ -2,7 +2,6 @@ import 'package:countries_app/constants.dart';
 import 'package:countries_app/screens/appointments/widgets/appointment_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
-
 import '../../models/apointments_model.dart';
 import 'appointment_bloc.dart';
 import 'meeting_data_source.dart';
@@ -17,16 +16,19 @@ class AppointmentsScreen extends StatefulWidget {
 
 class _AppointmentsScreenState extends State<AppointmentsScreen> {
   AppointmentsBloc appBloc = AppointmentsBloc();
+
   MeetingDataSource dataSource = MeetingDataSource(AppointmentsModel());
 
   @override
   void initState() {
     super.initState();
+
     fetchAppointments();
   }
 
   fetchAppointments() async {
     final appointmentsModel = await appBloc.getAppointments();
+
     setState(() {
       dataSource = MeetingDataSource(appointmentsModel);
     });
@@ -38,52 +40,53 @@ class _AppointmentsScreenState extends State<AppointmentsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  AppLocalizations.of(context)!.appointText,
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                IconButton(
-                    onPressed: () async {
-                      await fetchAppointments();
-                    },
-                    icon: const Icon(Icons.refresh))
-              ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!.appointText,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                      onPressed: () async {
+                        await fetchAppointments();
+                      },
+                      icon: const Icon(Icons.refresh))
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: MediaQuery.sizeOf(context).height * .55,
-            child: SfCalendar(
-              todayHighlightColor: primaryColor,
-              cellBorderColor: secendaryColor,
-              view: CalendarView.month,
-              dataSource: dataSource,
-              onTap: (calendarTapDetails) async {
-                if (calendarTapDetails.targetElement ==
-                    CalendarElement.appointment) {
-                  AppointmentsModel allApp = await fetchAppointments();
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * .55,
+              child: SfCalendar(
+                todayHighlightColor: primaryColor,
 
-                  final Appointment appointment =
-                      calendarTapDetails.appointments!.first;
-                  final matchingAppoint = allApp.data!.firstWhere(
-                    (app) =>
-                        app.dateFrom == appointment.startTime &&
-                        app.dateTo == appointment.endTime,
-                  );
-                  if (context.mounted) {
-                    showAppoitmentDetails(
-                      context: context,
-                      appoint: matchingAppoint,
-                      bloc: appBloc,
+                cellBorderColor: secendaryColor,
+
+                view: CalendarView.month,
+
+                dataSource: dataSource,
+
+                onTap: (calendarTapDetails) async {
+                  if (calendarTapDetails.targetElement ==
+                      CalendarElement.appointment) {
+                    AppointmentsModel allApp = await fetchAppointments();
+
+                    final Appointment appointment =
+                        calendarTapDetails.appointments!.first;
+
+                    final matchingAppoint = allApp.data!.firstWhere(
+                      (app) =>
+                          app.dateFrom == appointment.startTime &&
+                          app.dateTo == appointment.endTime,
                     );
-                    if (matchingAppoint != null) {
+
+                    if (context.mounted) {
                       showAppoitmentDetails(
                         context: context,
                         appoint: matchingAppoint,
@@ -93,10 +96,11 @@ class _AppointmentsScreenState extends State<AppointmentsScreen> {
                   }
                 },
 
-              monthViewSettings: MonthViewSettings(
-                  showAgenda: true,
-                  appointmentDisplayCount: dataSource.appointments!
-                      .length), // display count length of list on this date
+                monthViewSettings: MonthViewSettings(
+                    showAgenda: true,
+                    appointmentDisplayCount: dataSource.appointments!
+                        .length), // display count length of list on this date
+              ),
             ),
           ],
         ),
