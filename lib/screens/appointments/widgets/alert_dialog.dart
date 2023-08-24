6e9f82b9997aefa164635afa78911d84faf709f1
Danya <danya@JOD0271L.local>
2023-08-24 10:20:43 +0300

    passed functionto alertwidget instead of bloc

--- lib/screens/appointments/widgets/alert_dialog.dart
+++ lib/screens/appointments/widgets/alert_dialog.dart
@@ -1,11 +1,10 @@
-import 'package:countries_app/screens/appointments/appointment_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+typedef FutureCallback = Future<void> Function();
 Future<void> showAlertDialog(
-    {required BuildContext context,
-    required AppointmentsBloc bloc,
-    required int appointmentID}) async {
+    {required VoidCallback onCancelPressed,
+    required BuildContext context}) async {
   return showDialog<void>(
     context: context,
     barrierDismissible: false, // user must tap button!
@@ -29,7 +28,7 @@ Future<void> showAlertDialog(
           TextButton(
             child: Text(AppLocalizations.of(context)!.yesText),
             onPressed: () {
-              bloc.cancelAppointment(appointmentID);
+              onCancelPressed();
               int count = 0;
               Navigator.of(context).popUntil((_) => count++ >= 2);
             },
