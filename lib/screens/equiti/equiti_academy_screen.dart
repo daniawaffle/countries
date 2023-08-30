95ea78bb9f0d39b6704599f083a6a57b3de0706e
Eleana <eleana@JOD0272L.local>
2023-08-30 09:11:25 +0300

    equiti screen checked

--- lib/screens/equiti/equiti_academy_screen.dart
+++ lib/screens/equiti/equiti_academy_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import '../../constants.dart';
 
 import 'equiti_bloc.dart';
-import 'mentor_list_widget.dart';
+import 'widgets/mentor_list_widget.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class EquitiAcademyScreen extends StatefulWidget {
@@ -38,56 +38,60 @@ class _EquitiAcademyScreenState extends State<EquitiAcademyScreen> {
             child: StreamBuilder(
                 stream: bloc.categoriesStreamController.stream,
                 builder: (context, snapshot) {
-                  return ListView.builder(
-                    itemCount: bloc.categories.length,
-                    itemBuilder: (context, index) {
-                      bool isSelected = index == bloc.selectedItemIndex;
+                  if (snapshot.data != null) {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        bool isSelected = index == bloc.selectedItemIndex;
 
-                      return Card(
-                          color: isSelected
-                              ? AppConstants.selectedItemColor
-                              : Colors.white,
-                          elevation: 4,
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 5, vertical: 10),
-                          child: InkWell(
-                            onTap: () async {
-                              setState(() {
-                                bloc.selectedItemIndex =
-                                    index; // Update the selected index
-                              });
-                              await bloc.getMentors(bloc.categories[index].id!);
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  CircleAvatar(
-                                    radius: 25,
-                                    backgroundColor:
-                                        AppConstants.secendaryColor,
-                                    backgroundImage: NetworkImage(
-                                      AppConstants.categoryImageBaseUrl +
-                                          bloc.categories[index].icon!,
+                        return Card(
+                            color: isSelected
+                                ? AppConstants.selectedItemColor
+                                : Colors.white,
+                            elevation: 4,
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 5, vertical: 10),
+                            child: InkWell(
+                              onTap: () async {
+                                setState(() {
+                                  bloc.selectedItemIndex = index;
+                                });
+                                await bloc
+                                    .getMentors(snapshot.data![index].id!);
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 25,
+                                      backgroundColor:
+                                          AppConstants.secendaryColor,
+                                      backgroundImage: NetworkImage(
+                                        AppConstants.categoryImageBaseUrl +
+                                            snapshot.data![index].icon!,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(
-                                    height: 5,
-                                  ),
-                                  Text(
-                                    bloc.categories[index].name!,
-                                    style: const TextStyle(
-                                      fontSize: 14,
+                                    const SizedBox(
+                                      height: 5,
                                     ),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ],
+                                    Text(
+                                      snapshot.data![index].name!,
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          ));
-                    },
-                  );
+                            ));
+                      },
+                    );
+                  }
+
+                  return const CircularProgressIndicator();
                 }),
           ),
           Expanded(
